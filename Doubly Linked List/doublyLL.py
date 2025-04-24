f30f45a39bd6ff455a5ebfeebeeb89af97c633cd
Matheus Farias <matheusfarias3456@hotmail.com>
2025-04-24 19:07:43 -0300

    DLL Append method

--- Doubly Linked List/doublyLL.py
+++ Doubly Linked List/doublyLL.py
@@ -17,5 +17,23 @@ class DoublyLinkedList:
             print(temp.value)
             temp =  temp.next
 
+    def append(self, value):
+        new_node = Node(value)
+
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+
+        self.lengh += 1
+        return True
+
 doublyLL = DoublyLinkedList(7)
+doublyLL.append(5)
 doublyLL.print_list()
+print(F"HEAD = {doublyLL.head.value}")
+print(F"TAIL = {doublyLL.tail.value}")
+print(F"LENGHT = {doublyLL.lengh}")
